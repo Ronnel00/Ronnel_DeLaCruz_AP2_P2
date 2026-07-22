bc90dd206e87939ef2e681b76cc71caa392e117d
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-07-21 20:47:23 -0400

    Crea el App del Proyecto

---
+++ app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p2/Ronnel_DeLaCruz_AP2_P2App.kt
@@ -0,0 +1,7 @@
+package edu.ucne.ronnel_delacruz_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Ronnel_DeLaCruz_AP2_P2App : Application()
