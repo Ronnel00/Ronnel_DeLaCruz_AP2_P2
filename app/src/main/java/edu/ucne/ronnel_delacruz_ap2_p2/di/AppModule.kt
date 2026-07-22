4c34d13e1bcd6de2a5cf3fa9506a2401397d4292
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-07-21 20:47:08 -0400

    Crea el AppModule con los dos Primeros Metodos

---
+++ app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p2/di/AppModule.kt
@@ -0,0 +1,25 @@
+package edu.ucne.ronnel_delacruz_ap2_p2.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApi(moshi: Moshi) {
+    }
+}
