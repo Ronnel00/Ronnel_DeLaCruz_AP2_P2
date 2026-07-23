b20775add0fb44198ab8b3555cbaf1070fc764f2
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-07-22 20:10:06 -0400

    Crea el UiState del Detail

--- app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p2/presentation/detail/DetailUiState.kt
+++ app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p2/presentation/detail/DetailUiState.kt
@@ -1,4 +1,19 @@
 package edu.ucne.ronnel_delacruz_ap2_p2.presentation.detail
 
-class DetailUiState {
-}
+data class DetailUiState(
+    val gastoId: Int = 0,
+    val fecha: String = "",
+    val suplidor: String = "",
+    val ncf: String = "",
+    val itbis: Double = 0.0,
+    val monto: Double = 0.0,
+    val isLoading: Boolean = false,
+    val isSaving: Boolean = false,
+    val error: String? = null,
+    val savedSuccessfully: Boolean = false,
+    val suplidorError: String? = null,
+    val fechaError: String? = null,
+    val ncfError: String? = null,
+    val itbisError: String? = null,
+    val montoError: String? = null
+)
