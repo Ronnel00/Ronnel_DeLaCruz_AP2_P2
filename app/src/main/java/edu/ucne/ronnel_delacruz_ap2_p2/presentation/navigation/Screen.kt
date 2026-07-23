a88beeb2c6553218b38517f6c04f6d7d090c8c85
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-07-22 20:14:10 -0400

    Agrega las pantallas de navegación con Navigation 3

--- app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p2/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p2/presentation/navigation/Screen.kt
@@ -1,4 +1,14 @@
 package edu.ucne.ronnel_delacruz_ap2_p2.presentation.navigation
 
-class Screen {
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class Screen : NavKey {
+
+    @Serializable
+    data object GastoList : Screen()
+
+    @Serializable
+    data class GastoDetail(val id: Int = 0) : Screen()
 }
